5933c1786fd4cbc3f5ccb541fbe6c1d6816d59bd
Martin <tahiraj.martin@gmail.com>
2025-05-06 01:12:53 +0200

    change to debian base

--- minio/run.sh
+++ minio/run.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 set -e
-bashio::log.info "Run script started"
+echo "[DEBUG] Run script started"
+
+CONFIG="/data/options.json"
 
 # Config via HA options
-ACCESS_KEY=$(bashio::config 'access_key')
-SECRET_KEY=$(bashio::config 'secret_key')
-REGION=$(bashio::config 'region')
-BUCKET=$(bashio::config 'bucket')
+ACCESS_KEY=$(jq -r .access_key  "$CONFIG")
+SECRET_KEY=$(jq -r .secret_key  "$CONFIG")
+REGION=$(jq -r .region       "$CONFIG")
+BUCKET=$(jq -r .bucket       "$CONFIG")
+
 
 # Config via env vars (for docker container)
-export MINIO_ROOT_USER="${ACCESS_KEY}"
-export MINIO_ROOT_PASSWORD="${SECRET_KEY}"
+export MINIO_ROOT_USER="$ACCESS_KEY"
+export MINIO_ROOT_PASSWORD="$SECRET_KEY"
 export MINIO_REGION="${REGION:-us-east-1}"
 
 
@@ -19,19 +22,20 @@ CERT_PATH="/ssl/cert.pem"
 KEY_PATH="/ssl/key.pem"
 
 # First-run: make sure bucket exists (done via client)
-DATA_DIR="/data/${BUCKET}"
-mkdir -p "${DATA_DIR}"
+DATA_DIR="/data/$BUCKET"
+mkdir -p "$DATA_DIR"
 
-bashio::log.info "Starting MinIO (user: ${ACCESS_KEY}, region: ${MINIO_REGION}, bucket: ${BUCKET})"
-if bashio::fs.file_exists '/ssl/cert.pem' && bashio::fs.file_exists '/ssl/key.pem'; then
-  bashio::log.info "TLS cert found, launching HTTPS"
-  exec minio server "${DATA_DIR}" \
+echo "[INFO] Starting MinIO (user: $MINIO_ROOT_USER, region: $MINIO_REGION, bucket: $BUCKET)"
+
+if [[ -f "$CERT_PATH" && -f "$KEY_PATH" ]]; then
+  echo "[INFO] TLS cert found, launching in HTTPS mode"
+  exec minio server "$DATA_DIR" \
        --address ":9000" \
        --console-address ":9001" \
        --certs-dir /ssl
 else
-  bashio::log.info "Launching HTTP"
-  exec minio server "${DATA_DIR}" \
+  echo "[INFO] Launching in HTTP mode"
+  exec minio server "$DATA_DIR" \
        --address ":9000" \
        --console-address ":9001"
 fi
